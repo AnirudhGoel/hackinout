e832b194ed0e8583683593edb58d99ed960a3623
Anirudh Goel <Anirudhgoel.delhi@gmail.com>
2016-08-13 01:10:23 +0530

    Initial Commit

---
+++ API/inc/connection.inc.php
@@ -0,0 +1,14 @@
+<?php
+	
+/**
+ * @Author: anirudh
+ * @Date:   2016-08-12 8:45:00
+ * @Last Modified by:   Anirudh Goel
+ * @Last Modified time: 2016-08-12 8:45:00
+ */
+
+	$host = "localhost";
+	$password = "password";
+	$db = "health";
+
+?>
